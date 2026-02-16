e87ec875b802fa9cbac1b5693dc9aa81dfeb65b4
Zamit <cipherservices@gmail.com>
2026-02-16 18:42:37 +0100

    modif worker.py 2

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -1,4 +1,13 @@
 import os
+import sys
+from pathlib import Path
+
+# 🔥 Ajoute le backend au PYTHONPATH
+BASE_DIR = Path(__file__).resolve().parent
+BACKEND_DIR = BASE_DIR.parent  # remonte d’un niveau si besoin
+
+sys.path.append(str(BACKEND_DIR))
+
 from redis import Redis
 from rq import Worker, Queue
 
@@ -9,10 +18,6 @@ if not redis_url:
 redis_conn = Redis.from_url(redis_url)
 
 if __name__ == "__main__":
-    print("🧹 Clearing old jobs...")
-    queue = Queue("emails", connection=redis_conn)
-    queue.empty()
-
     print("🚀 RQ Worker started — listening on 'emails'")
     worker = Worker(["emails"], connection=redis_conn)
     worker.work()
