a328806e13e5790df8fad07f6d2abe3d1cea29d9
Zamit <cipherservices@gmail.com>
2026-02-16 18:33:47 +0100

    modif worker.py

--- backend/app/worker.py
+++ backend/app/worker.py
@@ -1,8 +1,7 @@
 import os
 from redis import Redis
-from rq import Worker
+from rq import Worker, Queue
 
-# Connexion Redis
 redis_url = os.getenv("REDIS_URL")
 if not redis_url:
     raise RuntimeError("REDIS_URL not set")
@@ -10,6 +9,10 @@ if not redis_url:
 redis_conn = Redis.from_url(redis_url)
 
 if __name__ == "__main__":
+    print("🧹 Clearing old jobs...")
+    queue = Queue("emails", connection=redis_conn)
+    queue.empty()
+
     print("🚀 RQ Worker started — listening on 'emails'")
     worker = Worker(["emails"], connection=redis_conn)
     worker.work()
